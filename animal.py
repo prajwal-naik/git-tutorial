ef7c714bdd18d26702b14a7a56868505ea1a9565
Naik <prajwal-k.naik@hpe.com>
2022-01-28 12:08:10 +0530

    Add dog functionality

--- animal.py
+++ animal.py
@@ -1,9 +1,16 @@
 import sys 
+
 def default():
     print("Hello")
 
+def dog():
+    print("woof")
+
 def main():
-    default()
+    if(sys.argv[1] == "dog"):
+        dog()
+    else:
+        default()
 
 if __name__ == "__main__":
     main()
